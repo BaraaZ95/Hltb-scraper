40fd696282f616f5bac60875d3d7dee11a1f79df
BaraaZ95 <80360467+BaraaZ95@users.noreply.github.com>
2023-02-03 10:54:12 +0200

    updates and fixes

--- howlongtobeat/spiders/hltb.py
+++ howlongtobeat/spiders/hltb.py
@@ -2,7 +2,11 @@ import scrapy
 from urllib.parse import urljoin
 import json
 
-
+headers = {
+                "content-type":"application/json",
+                "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Mobile Safari/537.36", 
+                "referer": "https://howlongtobeat.com/"
+            }
 
 class HltbSpider(scrapy.Spider):
     name = 'hltb'
@@ -11,20 +15,10 @@ class HltbSpider(scrapy.Spider):
         for page in range(1, 3038):                
             url = 'https://howlongtobeat.com/api/search'
             payload = f'{{"searchType":"games","searchTerms":[""],"searchPage":{page},"size":20,"searchOptions":{{"games":{{"userId":0,"platform":"","sortCategory":"popular","rangeCategory":"main","rangeTime":{{"min":null,"max":null}},"gameplay":{{"perspective":"","flow":"","genre":""}},"rangeYear":{{"min":"","max":""}},"modifier":""}},"users":{{"sortCategory":"postcount"}},"filter":"","sort":0,"randomizer":0}}}}'
-            headers = {
-                "content-type":"application/json",
-                "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Mobile Safari/537.36", 
-                "referer": "https://howlongtobeat.com/"
-            }
+            
             yield scrapy.Request(url=url, method='POST', body=payload, headers=headers, callback=self.parse_urls)
 
-    def parse_urls(self, response):
-        headers = {
-                "content-type":"application/json",
-                "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Mobile Safari/537.36", 
-                "referer": "https://howlongtobeat.com/"
-            }
-        
+    def parse_urls(self, response):        
         json_data = json.loads(response.text)
         games = json_data['data']
         for game in games: 
@@ -39,24 +33,29 @@ class HltbSpider(scrapy.Spider):
         #This parses all the the tables and stats and extracts all the info from them
         Name = response.css('div[class="GameHeader_profile_header__g1fEv shadow_text"]::text').get()
         info_dict = {"Name": Name}
+        info_dict['Stats'] = {}
         
         tables =  response.css('table')
         for table in tables:
             columns = table.css('thead>tr>td::text').getall()
+            dict_ = {columns[0]:{}}
             rows =  table.css('tbody> tr')
             for row in rows:
                 row_name = row.css('::text').get()
                 td = row.css('td::text').getall()
-                info_dict.update({columns[0]:{row_name: dict(zip(columns[1:], td[1:]))}})
-                
-        info_list =  response.css('div[class="GameReviewRoundUp_featured__u3vuA"] > div > div > h5 ::text').getall()
-        
+                dict_[columns[0]].update({row_name: dict(zip(columns[1:], td[1:]))})
+            info_dict['Stats'].update(dict_)
+            
+        info_list = response.css('div[class="GameHeader_profile_details__QMxb9"] >ul>li::text').getall()
         try: 
             Rating_index = info_list.index('Rating') - 1
-            Retirement_index= info_list.index('Retirement')- 2
-            info_dict.update(
-                {"Rating": info_list[Rating_index], "Retirement_Rate": ' '.join(info_list[Retirement_index])})
-        except: pass    
+            info_dict.update({"Rating": info_list[Rating_index]})
+        except: pass
+        try:
+            Retirement_index= info_list.index('Retirement')- 1
+            info_dict.update({"Retirement_Rate": info_list[Retirement_index]})
+        except: pass
+         
         
         steam_url = response.css('div[class="GameSummary_profile_info__e935c"] >strong> a::attr(href)').get()
         if steam_url:
